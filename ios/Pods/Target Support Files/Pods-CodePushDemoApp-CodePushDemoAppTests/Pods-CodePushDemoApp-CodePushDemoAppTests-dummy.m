797e40f85acda36f1d46bcaca1b98982c1a59fbe
AIKAWA Satoshi <saikawa3456@gmail.com>
2023-01-27 22:11:14 +0900

    tc

---
+++ ios/Pods/Target Support Files/Pods-CodePushDemoApp-CodePushDemoAppTests/Pods-CodePushDemoApp-CodePushDemoAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodePushDemoApp_CodePushDemoAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_CodePushDemoApp_CodePushDemoAppTests
+@end
